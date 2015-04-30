1ae023dc84a980aa380e1d09d327456a25595c40
montimaj <monti.majumdar@gmail.com>
2015-04-30 17:32:07 +0530

    PC modules successfully created

---
+++ GenKeys.java
@@ -0,0 +1,78 @@
+import java.io.FileOutputStream;
+import java.io.File;
+
+import edu.sxccal.utilities.Log;
+
+import java.security.SecureRandom;
+import java.security.KeyPairGenerator;
+import java.security.Signature;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class GenKeys 
+{
+  
+  public static void main(String args[]) throws Exception
+  {
+	    Process p1=null;
+	    try
+	    {	      
+	      File dir=new File("KEYS");
+	      if(!dir.exists())
+		dir.mkdir();
+	      File key1=new File("KEYS/pubkey"), key2=new File("KEYS/privkey"); 
+	      if(key1.exists() || key2.exists())
+	      {		
+		String[] x={"zenity","--info","--text=Keys have already been generated!\nTo regenerate keys delete previous keys in the KEYS directory"};
+		p1=new ProcessBuilder(x).start();
+		p1.waitFor();
+	      }
+	      else
+	      {
+		String[] x={"zenity","--progress","--pulsate","--no-cancel","--text=Generating Keys..."};
+		p1=new ProcessBuilder(x).start();
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "SunRsaSign"); //Generate a key pair
+		SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+		keyGen.initialize(3072, random); //initialize a 3072-bit key with a SecureRandom object
+		KeyPair pair = keyGen.generateKeyPair(); //generate private and public key
+		PrivateKey priv = pair.getPrivate();
+		PublicKey pub = pair.getPublic();
+		byte[] pubkey = pub.getEncoded(), privkey=priv.getEncoded();		
+		  
+		//Write public and private keys to files
+		FileOutputStream keyfos = new FileOutputStream(key1); 
+		keyfos.write(pubkey);
+		keyfos.close();  	  
+		keyfos=new FileOutputStream(key2);
+		keyfos.write(privkey);
+		keyfos.close();	
+		String x1[]={"zenity","--info","--text=Keys have been written to "+dir.getAbsolutePath()};
+		p1.destroy();
+		p1=new ProcessBuilder(x1).start();
+		p1.waitFor();
+	      }
+	    }
+	    catch(Exception e)
+	    {
+	      String s=Log.create_log(e);
+	      String[] x={"zenity","--error","--text="+s};
+	      p1=new ProcessBuilder(x).start(); //Show error window
+	      p1.waitFor();
+	    }	    
+  }
+}
+
+/*		***		LIBRARY OVERVIEW	***	                              		*/
+
+/*SecureRandom: provides a cryptographically strong random number generator (RNG). 
+  Class Details: https://docs.oracle.com/javase/7/docs/api/java/security/SecureRandom.html
+
+  KeyPairGenerator: Generates pairs of public and private keys.
+  KeyPairGenerator.initialize(int keysize, SecureRandom random): Initializes the key pair generator 
+  for a certain keysize with the given source of randomness 
+  Class Details: https://docs.oracle.com/javase/7/docs/api/java/security/KeyPairGenerator.html  
+
+  KeyPair: Simple holder for a key pair (a public key and a private key).
+  Class Details: https://docs.oracle.com/javase/7/docs/api/java/security/KeyPair.html
+*/
---
+++ com/oracle/GenSig.java
@@ -0,0 +1,61 @@
+package com.oracle;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.BufferedInputStream;
+
+import java.security.SecureRandom;
+import java.security.KeyPairGenerator;
+import java.security.KeyFactory;
+import java.security.Signature;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+/* This is the main module that creates the detached digital signature and the public key */
+//Functionality is same to that of the android app module com.oracle.android.GenSig
+public class GenSig
+{
+    public static void Gen_sig(String file,String dest) throws Exception
+    {	   
+            
+            /* Create a Signature object and initialize it with the private key */
+            Signature rsa = Signature.getInstance("SHA1withRSA", "SunRsaSign");
+            FileInputStream keyfis = new FileInputStream("KEYS/privkey"); //import encoded private key
+            byte[] encKey = new byte[keyfis.available()];  
+            keyfis.read(encKey);
+            keyfis.close();
+            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(encKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA", "SunRsaSign");
+            // Generate private key object from the provided key specification(key material)
+            PrivateKey priv = keyFactory.generatePrivate(privKeySpec); 
+            rsa.initSign(priv); 
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bufin = new BufferedInputStream(fis);
+            byte[] buffer = new byte[3072];
+            int len;
+            while (bufin.available() != 0) //update input file
+            {
+                len = bufin.read(buffer);
+                rsa.update(buffer, 0, len);
+            }
+            bufin.close();
+            byte[] realSig = rsa.sign(); //Now that all the data to be signed has been read in,generate a signature for it     
+            FileOutputStream sigfos = new FileOutputStream(dest+"/sig"); //create 'sig' file
+            sigfos.write(realSig);//write the signature to 'sig' file
+            sigfos.close();            	  
+    }
+}
+/*		***		LIBRARY OVERVIEW	***	                              		*/
+
+/*Signature: Used to provide applications the functionality of a digital signature algorithm
+  Class Details: https://docs.oracle.com/javase/7/docs/api/java/security/Signature.html
+  
+  PKCS8EncodedKeySpec: This class represents the ASN.1 encoding of a private key, 
+  encoded according to the ASN.1 type PrivateKeyInfo
+  Class Details: https://docs.oracle.com/javase/7/docs/api/java/security/spec/PKCS8EncodedKeySpec.html
+  
+  BufferedInputStream: A BufferedInputStream adds functionality to another input stream
+  Class Details: docs.oracle.com/javase/7/docs/api/java/io/BufferedInputStream.html
+*/
